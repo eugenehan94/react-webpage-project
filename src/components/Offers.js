b26293fb592d1848dd7cb8030f22ec0ed2422688
eugenehan94 <eugene.han94@gmail.com>
2021-02-24 22:26:09 -0500

    updated component

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -4,7 +4,9 @@ import Offer from "./Offer";
 const Offers = ({ offerList, removeOffer }) => {
   return (
     <section>
-      <div>{offerList.length === 0 ? null : <h1>Offers</h1>}</div>
+      <div className="offer-heading">
+        <h1>{offerList.length === 0 ? null : <h1>Offers</h1>} </h1>
+      </div>
       <div className="offers-container">
         {offerList.map((offer) => {
           return <Offer key={offer.id} removeOffer={removeOffer} {...offer} />;
